699cfc911b3295370028e8a0f0b7b6d18e7f62d3
Regis-Ntwari <rex90danny@gmail.com>
2023-10-18 16:41:20 +0200

    fix mobile view and finish up the portfolio

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import './portofolio.css';
+import React from "react";
+import "./portofolio.css";
+import IMG from "../../assets/portfolio.jpg";
 
 const Portofolio = () => {
+  const projects = [
+    {
+      id: 1,
+      image: IMG,
+      title: "Project 1",
+      github: "https://www.github.com",
+      liveLink: "https://www.dribbble.com",
+    },
+    {
+      id: 2,
+      image: IMG,
+      title: "Project 2",
+      github: "https://www.github.com",
+      liveLink: "https://www.dribbble.com",
+    },
+    {
+      id: 3,
+      image: IMG,
+      title: "Project 3",
+      github: "https://www.github.com",
+      liveLink: "https://www.dribbble.com",
+    },
+  ];
   return (
-    <section id='portofolio'>
-      portofolio
-    </section>
-  )
-}
+    <section id="portofolio">
+      <h5>My Recent Work</h5>
+      <h2>Portofolio</h2>
+      <div className="container portfolio__container">
+        {projects.map((project) => {
+          return (
+            <article key={project.id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={project.image} alt="" />
+              </div>
 
-export default Portofolio
+              <h3>{project.title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={project.github}
+                  className="btn "
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  href={project.liveLink}
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portofolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import './testimonials.css';
+import React from "react";
+
+import { Pagination, Scrollbar} from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import "./testimonials.css";
+import avatar from "../../assets/avatar-boy.jpg";
 
 const Testimonials = () => {
+  const reviews = [
+    {
+      id: 1,
+      name: 'Tiana Teta',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis eveniet amet explicabo quam eos aperiam error beatae accusamus totam ratione incidunt voluptas deserunt aliquid porro saepe,nesciunt cumque, tempore aut.'
+    },
+    {
+      id: 2,
+      name: 'Leo Ngabo',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis eveniet amet explicabo quam eos aperiam error beatae accusamus totam ratione incidunt voluptas deserunt aliquid porro saepe,nesciunt cumque, tempore aut.'
+    },
+    {
+      id: 3,
+      name: 'Regis Ntwari',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis eveniet amet explicabo quam eos aperiam error beatae accusamus totam ratione incidunt voluptas deserunt aliquid porro saepe,nesciunt cumque, tempore aut.'
+    }
+  ];
   return (
-    <section id='testimonials'>
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
 
+      <Swiper className="container testimonials__container" 
+        modules={[Pagination, Scrollbar]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{clickable: true}}
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar} alt="avatar image" />
+          </div>
+          <h5 className="client__name">{review.name}</h5>
+            <small className="client__review">
+              {review.review}
+            </small>
+        </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
